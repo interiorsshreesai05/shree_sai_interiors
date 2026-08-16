f877c9c4747b6d2df77e5332dd231e7f42cb2f5f
Vaibhav <sanapvaibhav8767@gmail.com>
2026-08-16 18:26:58 +0530

    Done

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -169,7 +169,7 @@ export default function About() {
             >
               <div className={styles.imageFramePrimary}>
                 <Image
-                  src="/abtouimage.png"
+                  src="/aboutimage.png"
                   alt="Modern interior design showcase"
                   fill
                   sizes="(max-width: 1024px) 100vw, 40vw"
